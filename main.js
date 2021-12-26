d9f3efa38eb76ce29e8de259f26b7d9d6d46ca42
root <bobwatcherx@gmail.com>
2021-12-26 14:55:26 +0700

    railway

---
+++ main.js
@@ -0,0 +1,8 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World from Railway';
+});
+const PORT = 5000
+app.listen(process.env.PORT || PORT,()=>console.log("run server " + PORT));
